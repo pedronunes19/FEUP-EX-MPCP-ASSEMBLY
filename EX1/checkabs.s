86d07de8f54ccc19e5c12ad1e4709bf246716040
Pedro Nunes <75331417+pedronunes19@users.noreply.github.com>
2021-06-03 17:58:20 +0100

    Update checkabs.s

--- EX1/checkabs.s
+++ EX1/checkabs.s
@@ -3,19 +3,19 @@
 .type CheckABS, "function"
 
 CheckABS:	mov X3, 0
-			mov W4, 0
-			mov W6, 0
+		mov W4, 0
+		mov W6, 0
 loop:		cbz W1, end
-			ldr W5, [X2, X3]
-			cmp W5, 0
-			csneg W5, W5, W5, GE
-			cmp W5, W0
-			b.LE skip
-			str W6, [X2, X3]
-			add W4, W4, 1
+		ldr W5, [X2, X3]
+		cmp W5, 0
+		csneg W5, W5, W5, GE
+		cmp W5, W0
+		b.LE skip
+		str W6, [X2, X3]
+		add W4, W4, 1
 skip:		add X3, X3, 4
-			sub W1, W1, 1
-			b loop
+		sub W1, W1, 1
+		b loop
 end:		mov W0, W4
-			ret
+		ret
 
