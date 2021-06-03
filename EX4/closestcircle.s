7fde5853dcd4006375cdd74b09c914aa3c8ea1b7
pedronunes19 <pedromiguelmnunes@gmail.com>
2021-06-03 18:02:02 +0100

    formating

--- EX4/closestcircle.s
+++ EX4/closestcircle.s
@@ -6,9 +6,9 @@ closestCircle:	mov X15, X3  // guardar coisas (sem usar stack)
 				mov W14, W2
 point_loop:		cbz W0, end
 				sub W0, W0, 1
-				mov X3, X15  // voltar ao início do array de círculos
+				mov X3, X15  // voltar ao inï¿½cio do array de cï¿½rculos
 				mov W2, W14  // devolver o valor original a NC
-				mov W9, 0  // indice círculos
+				mov W9, 0  // indice cï¿½rculos
 				ldp S1, S0, [X1], 8
 				ldp D3, D2, [X3], 16
 				ldr D4, [X3], 8
@@ -26,7 +26,7 @@ point_loop:		cbz W0, end
 				fmov D6, D5  // guardar atual "melhor" resultado, mesmo se for 0
 				fcmp D5, #0.0
 				b.ls skip1
-				str W9, [X4]  // guardar índice se for uma distância válida
+				str W9, [X4]  // guardar ï¿½ndice se for uma distï¿½ncia vï¿½lida
 skip1:			sub W2, W2, 1
 				add W9, W9, 1
 c_loop:			cbz W2, point_back
@@ -41,12 +41,12 @@ c_loop:			cbz W2, point_back
 				fsqrt D9, D9
 				fsub D5, D9, D4
 
-				fcmp D5, #0.0  // ignorar distâncias 0
+				fcmp D5, #0.0  // ignorar distï¿½ncias 0
 				b.ls skip2
 				fcmp D5, D6
 				b.gt skip2
 				fmov D6, D5  // guardar atual "melhor" resultado
-				str W9, [X4]  // guardar índice
+				str W9, [X4]  // guardar ï¿½ndice
 skip2:			sub W2, W2, 1
 				add W9, W9, 1
 				b c_loop
